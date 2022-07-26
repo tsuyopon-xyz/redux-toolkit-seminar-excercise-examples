72426e32c2845577da8c36fbdbf6938719ddf81d
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-07-27 01:21:20 +0900

    Done quick start in tutorial.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
