83f810b58e3684d31ccb8e14bdca7db4475451fe
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-07-30 19:44:22 +0900

    Impl a test for remove reducer

--- src/features/todos/todosSlice.spec.ts
+++ src/features/todos/todosSlice.spec.ts
@@ -1,15 +1,15 @@
-import todosReducer, { create, TodoState } from './todosSlice';
+import todosReducer, { create, remove, TodoState } from './todosSlice';
 import { TodoInput, TodoId } from './types';
 
 describe('todos reducer', () => {
-  const initialState: TodoState = {
-    todos: [],
-    displayStatus: 'all',
-    isFetching: false,
-    error: null,
-  };
-
   it('should handle create reducer', () => {
+    const initialState: TodoState = {
+      todos: [],
+      displayStatus: 'all',
+      isFetching: false,
+      error: null,
+    };
+
     const payload: TodoInput = {
       title: 'title1',
       body: 'body1',
@@ -32,5 +32,37 @@ describe('todos reducer', () => {
       isFetching: false,
       error: null,
     });
+  });
+
+  it('should handle remove', () => {
+    const targetId = 'test-id-1';
+    const initialState: TodoState = {
+      todos: [
+        {
+          id: targetId,
+          title: '',
+          body: '',
+          status: 'waiting',
+          createdAt: '2022-07-30 00:00:00',
+        },
+        {
+          id: 'test-id-2',
+          title: '',
+          body: '',
+          status: 'waiting',
+          createdAt: '2022-07-30 00:00:00',
+        },
+      ],
+      displayStatus: 'all',
+      isFetching: false,
+      error: null,
+    };
+
+    const payloadForRemove: TodoId = targetId;
+    const newState = todosReducer(initialState, remove(payloadForRemove));
+
+    expect(newState.todos.length).toEqual(2);
+    expect(newState.todos[0].deletedAt !== undefined).toEqual(true);
+    expect(newState.todos[1].deletedAt === undefined).toEqual(true);
   });
 });
