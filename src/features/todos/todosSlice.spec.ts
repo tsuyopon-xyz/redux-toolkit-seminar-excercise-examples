7e7601b93e2e6507176dfd0352e8f7daa36ea9f4
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-07-30 19:48:18 +0900

    Impl a test for update reducer

--- src/features/todos/todosSlice.spec.ts
+++ src/features/todos/todosSlice.spec.ts
@@ -1,5 +1,5 @@
-import todosReducer, { create, remove, TodoState } from './todosSlice';
-import { TodoInput, TodoId } from './types';
+import todosReducer, { create, remove, update, TodoState } from './todosSlice';
+import { TodoInput, TodoId, TodoUpdatePayload } from './types';
 
 describe('todos reducer', () => {
   it('should handle create reducer', () => {
@@ -34,7 +34,7 @@ describe('todos reducer', () => {
     });
   });
 
-  it('should handle remove', () => {
+  it('should handle remove reducer', () => {
     const targetId = 'test-id-1';
     const initialState: TodoState = {
       todos: [
@@ -64,5 +64,38 @@ describe('todos reducer', () => {
     expect(newState.todos.length).toEqual(2);
     expect(newState.todos[0].deletedAt !== undefined).toEqual(true);
     expect(newState.todos[1].deletedAt === undefined).toEqual(true);
+  });
+
+  it('should handle update reducer', () => {
+    const targetId = 'test-id-1';
+    const initialState: TodoState = {
+      todos: [
+        {
+          id: targetId,
+          title: '',
+          body: '',
+          status: 'waiting',
+          createdAt: '2022-07-30 00:00:00',
+        },
+      ],
+      displayStatus: 'all',
+      isFetching: false,
+      error: null,
+    };
+
+    const payloadForUpdate: TodoUpdatePayload = {
+      id: targetId,
+      input: {
+        title: 'updated title',
+        body: 'updated body',
+        status: 'completed',
+      },
+    };
+    const newState = todosReducer(initialState, update(payloadForUpdate));
+
+    expect(newState.todos[0].title).toEqual(payloadForUpdate.input.title);
+    expect(newState.todos[0].body).toEqual(payloadForUpdate.input.body);
+    expect(newState.todos[0].status).toEqual(payloadForUpdate.input.status);
+    expect(newState.todos[0].updatedAt !== undefined).toEqual(true);
   });
 });
